4564abedfeeff5bcc793b9af5c9c1e7b938ef5bf
Robert Knight <robert@microbit.org>
2024-10-09 09:40:23 +0100

    Update base path
    Ensures references use relative path

--- simx/vite.config.ts
+++ simx/vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set base for GH Pages
-  base: process.env.GITHUB_ACTIONS
-    ? "/makecode-microbit-ml-simulator/"
-    : undefined,
+  base: "./",
   build: {
     outDir: "dist",
     sourcemap: true,
