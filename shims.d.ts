380b1d48b2722afbd20db952e5b3ad0801e35a53
Robert Knight <robert@microbit.org>
2024-11-14 10:40:17 +0000

    Tweaks to jsdoc strings for translation
    These changes help the pxt gendocs tool pick up all strings.

--- shims.d.ts
+++ shims.d.ts
@@ -7,11 +7,11 @@ declare namespace mlrunner {
      * This custom version of the MakeCode onEvent function is needed due to:
      * https://github.com/microsoft/pxt-microbit/issues/5709
      * 
+     *
      * @param src The ID of the component to listen to.
      * @param value The event value to listen to from that component.
      * @param handler The function to call when the event is detected.
-     * @param flags The specified event flags are ignored and configured via
-     *              pxt.json.
+     * @param flags The specified event flags are ignored and configured via pxt.json.
      */
     //% flags.defl=0 shim=mlrunner::customOnEvent
     function customOnEvent(src: int32, value: int32, handler: () => void, flags?: int32): void;
