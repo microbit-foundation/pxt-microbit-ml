04737de5fece1f8d44ea0e1ef360bec6a48aedc9
Grace <grace@microbit.org>
2025-01-03 09:39:42 +0000

    Add update-translations script for sim

---
+++ simx/bin/update-translations.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Partial automation of updating sim and extension translations.
+#
+# Updates sim and extension translations. 
+# 
+# New languages require code change in below and in simx/src/messages/TranslationProvider.tsx.
+#
+
+set -euxo pipefail
+
+if [ $# -eq 0 ]; then
+  echo Missing argument to extracted Crowdin ZIP >&1
+  exit 1
+fi
+
+languages="es-ES ja ko nl pl pt-BR zh-TW"
+
+mkdir -p crowdin/translated
+for language in $languages; do
+    lower="${language,,}"
+    prefix="${1}/${language}"
+    cp "${prefix}/ui.en.json" "lang/ui.${lower}.json"
+done
+
+npm run i18n:compile
